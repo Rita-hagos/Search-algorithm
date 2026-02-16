ce74a7804649126b082357b2028d4c698948b2be
Rita-hagos <rhagos2038@sdsu.edu>
2026-02-15 17:34:08 -0800

    Fix search algorithms and correct syntax errors
    Fixes issues in linear and binary search implementations, including correcting syntax errors and improving logic.

--- search algo.cpp
+++ search algo.cpp
@@ -1,1 +1,56 @@
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//Linear Search
+int linearSearch (vector<int> & arr, int key, int& steps) {
+  steps = 0;
+for (int i =0; i < arr.size(); i++ {
+steps++;
+if (arr[i]== key)
+  return i;
+}
+
+//Binary Search
+int binarSearch (vector<int>& arr, int key, int& steps ) {
+  steps = 0;
+int low = 0;
+int high = arr.size () -1;
+
+while (low<- high) {
+int mid = (low +hugh) / 2;
+
+steps++;
+if (arr[mid] == key)
+  return mid;
+if (arr[mid] < key)
+  low = mid +1;
+else
+  high - mid - 1;
+}
+return -1;
+}
+
+int main () {
+  const int N = 100000;
+vector <int> arr(N);
+
+for (int i = 0, i < N; i++) {
+arr[i] = i;
+}
+int key;
+cout << "Enter number to search:" ;
+cin >> key;
+
+int linearSteps, binarySteps;
+
+linearSearch(arr, key, linearSteps);
+binarySearch (arr, key, binarySteps);
+
+cout << "\nLinear Search Steps:" << linearSteps << endl;
+cout << "Binary Search Steps;" << binarySteps << endl;
+
+return 0;
+}
